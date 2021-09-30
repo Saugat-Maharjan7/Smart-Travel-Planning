09b0ebee0c14b8f3b9df53b3d502cd06a076d5d8
Saugat-Maharjan7 <saugatmaharjan11@gmail.com>
2021-09-30 17:03:15 +0545

    STPA V7(login validation, realtime database, Maps working-7, autocomplete textfield, packages v2, topDestinations edited)

--- lib/Home/topDestinations.dart
+++ lib/Home/topDestinations.dart
@@ -57,165 +57,167 @@ class _DestinationScreenState extends State<DestinationScreen> {
                     ),
                   ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(icon: Icon(Icons.arrow_back,
-                    color: Colors.tealAccent,
-                    size: 30,),
-                    onPressed: () => Navigator.pop(context),
-                    ),
-
-                Row(
-                     children: [
-                   IconButton(icon: Icon(Icons.sort_outlined,
-                     color: Colors.tealAccent,
-                     size: 30,),
-                      onPressed: () => print('Sorting form'),
-                      ),
-                       ],
-                  ),
-                  ],
-                ),
-                Positioned(
-                  left: 20,
-                  bottom: 20,
-                  child: Column(
-                    crossAxisAlignment:
-                    CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            FlutterIcons.location_city_mdi,
-                            size: 30.0,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            widget.destination.city,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: widget.destination.activities.length,
-                  itemBuilder: (BuildContext context, int index){
-                    Activity activity = widget.destination.activities[index];
-                return Stack(
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.push(
-                          context, MaterialPageRoute(builder: (context)=> DetailPage(),)),
-                      child: Container(
-                        margin: EdgeInsets.fromLTRB(40, 5, 20, 5),
-                        height: 170,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.white30,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                          padding:  EdgeInsets.fromLTRB(100, 20, 20, 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    width: 120,
-                                    child: Text(activity.name,
-                                    style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    ),
-                                  ),
-                                  Text('\Rs.${activity.price}',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600,
-                                    ),),
-                                ],
-                              ),
-                              _buildRatingStars(activity.rating),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    width: 70,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blueAccent,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      activity.startTimes[0],
-                                    ),
-                                  ),
-                                  SizedBox(width: 10,),
-                                  Container(
-                                    width: 70,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blueAccent,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      activity.startTimes[1],
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Positioned(
-                      left:20,
-                      top: 15,
-                      bottom: 15,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image(
-                          width: 110,
-                            image: AssetImage(activity.imgUrl),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-
-                  ],
-                );
-              },
-              ),
-            ),
+            //     Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       children: [
+            //         IconButton(icon: Icon(Icons.arrow_back,
+            //         color: Colors.tealAccent,
+            //         size: 30,),
+            //         onPressed: () => Navigator.pop(context),
+            //         ),
+            //
+            //     Row(
+            //          children: [
+            //        IconButton(icon: Icon(Icons.sort_outlined,
+            //          color: Colors.tealAccent,
+            //          size: 30,),
+            //           onPressed: () => print('Sorting form'),
+            //           ),
+            //            ],
+            //       ),
+            //       ],
+            //     ),
+            //     Positioned(
+            //       left: 20,
+            //       bottom: 20,
+            //       child: Column(
+            //         crossAxisAlignment:
+            //         CrossAxisAlignment.start,
+            //         children: <Widget>[
+            //           Row(
+            //             children: <Widget>[
+            //               Icon(
+            //                 FlutterIcons.location_city_mdi,
+            //                 size: 30.0,
+            //                 color: Colors.white,
+            //               ),
+            //               SizedBox(
+            //                 width: 5,
+            //               ),
+            //               Text(
+            //                 widget.destination.city,
+            //                 style: TextStyle(
+            //                   color: Colors.white,
+            //                   fontSize: 30,
+            //                   fontWeight: FontWeight.w600,
+            //                   letterSpacing: 1.2,
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // Expanded(
+            //   child: ListView.builder(
+            //       itemCount: widget.destination.activities.length,
+            //       itemBuilder: (BuildContext context, int index){
+            //         Activity activity = widget.destination.activities[index];
+            //     return Stack(
+            //       children: [
+            //         GestureDetector(
+            //           onTap: () => Navigator.push(
+            //               context, MaterialPageRoute(builder: (context)=> DetailPage(),)),
+            //           child: Container(
+            //             margin: EdgeInsets.fromLTRB(40, 5, 20, 5),
+            //             height: 170,
+            //             width: double.infinity,
+            //             decoration: BoxDecoration(
+            //               color: Colors.white30,
+            //               borderRadius: BorderRadius.circular(20),
+            //             ),
+            //             child: Padding(
+            //               padding:  EdgeInsets.fromLTRB(100, 20, 20, 20),
+            //               child: Column(
+            //                 crossAxisAlignment: CrossAxisAlignment.start,
+            //                 mainAxisAlignment: MainAxisAlignment.center,
+            //                 children: [
+            //                   Row(
+            //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //                     crossAxisAlignment: CrossAxisAlignment.start,
+            //                     children: [
+            //                       Container(
+            //                         width: 120,
+            //                         child: Text(activity.name,
+            //                         style: TextStyle(
+            //                           fontSize: 22,
+            //                           fontWeight: FontWeight.bold,
+            //                         ),
+            //                         overflow: TextOverflow.ellipsis,
+            //                         maxLines: 2,
+            //                         ),
+            //                       ),
+            //                       Text('\Rs.${activity.price}',
+            //                         style: TextStyle(
+            //                           fontSize: 20,
+            //                           fontWeight: FontWeight.w600,
+            //                         ),),
+            //                     ],
+            //                   ),
+            //                   _buildRatingStars(activity.rating),
+            //                   SizedBox(
+            //                     height: 10,
+            //                   ),
+            //                   Row(
+            //                     children: [
+            //                       Container(
+            //                         width: 70,
+            //                         decoration: BoxDecoration(
+            //                           color: Colors.blueAccent,
+            //                           borderRadius: BorderRadius.circular(10),
+            //                         ),
+            //                         alignment: Alignment.center,
+            //                         child: Text(
+            //                           activity.startTimes[0],
+            //                         ),
+            //                       ),
+            //                       SizedBox(width: 10,),
+            //                       Container(
+            //                         width: 70,
+            //                         decoration: BoxDecoration(
+            //                           color: Colors.blueAccent,
+            //                           borderRadius: BorderRadius.circular(10),
+            //                         ),
+            //                         alignment: Alignment.center,
+            //                         child: Text(
+            //                           activity.startTimes[1],
+            //                         ),
+            //                       ),
+            //                     ],
+            //                   ),
+            //
+            //                 ],
+            //               ),
+            //             ),
+            //           ),
+            //         ),
+            //
+            //         Positioned(
+            //           left:20,
+            //           top: 15,
+            //           bottom: 15,
+            //           child: ClipRRect(
+            //             borderRadius: BorderRadius.circular(20),
+            //             child: Image(
+            //               width: 110,
+            //                 image: AssetImage(activity.imgUrl),
+            //               fit: BoxFit.cover,
+            //             ),
+            //           ),
+            //         ),
+            //
+            //       ],
+            //     );
+            //   },
+            //   ),
+            // ),
           ],
         ),
-      ),
+      ],
+    ),
+    ),
     );
   }
 }
